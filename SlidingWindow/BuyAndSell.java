4f95185f285926b718da15edf5e0dff48b5f1f63
Sarah Zwart <scz225@lehigh.edu>
2023-09-17 18:29:50 -0400

    Progress on BuyAndSell

--- SlidingWindow/BuyAndSell.java
+++ SlidingWindow/BuyAndSell.java
@@ -5,7 +5,15 @@ public class BuyAndSell {
     achieve from this transaction. If you cannot achieve profit return 0;
     */
     public static int maxProfit(int[] prices){
-        for()
+        int min = prices[0];
+        int maxGreaterThanMin = prices[1];
+        int profit = 0;
+        for(int i = 0; i < prices.length; i++){
+            if(prices[i] < min){
+
+            }
+            
+        }
     }
 
     public static void main (String [] args){
