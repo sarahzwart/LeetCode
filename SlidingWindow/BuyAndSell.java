d594fb6ba2c77acfedad31f435a5ec7b3287f3de
Sarah Zwart <scz225@lehigh.edu>
2023-09-17 15:35:10 -0400

    BuyAndSell.java challenge

---
+++ SlidingWindow/BuyAndSell.java
@@ -0,0 +1,17 @@
+public class BuyAndSell {
+    /*You are given an array prices where prices[i] is the given stock on the ith day
+    You want to maximize your profit by choosing a single day to buy one stock and choosing
+    a different day in the future to sell that stock. Return the maximum profit you can
+    achieve from this transaction. If you cannot achieve profit return 0;
+    */
+    public static int maxProfit(int[] prices){
+        for()
+    }
+
+    public static void main (String [] args){
+        int[] prices1 =  {2,5,3,7,9};
+        maxProfit(prices1);
+
+    }
+}
+
