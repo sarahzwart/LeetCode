a861be06cbf63f6e3740e97ab6a78a794aad2a32
Sarah Zwart <scz225@lehigh.edu>
2023-10-02 18:40:34 -0400

    RemoveColor Game in Python

---
+++ ChallengeOfTheDay/RemoveColoredPieces.py
@@ -0,0 +1,18 @@
+class Solution:
+    def winnerOfGame(colors: str) -> bool:
+        n =len(colors)
+        aCount = 0
+        bCount = 0
+        if n  < 3:
+            return False
+        for i in range(1,n-1):
+            if colors[i]==colors[i-1]==colors[i+1]:
+                if colors[i]=='A':
+                    aCount+=1
+                else:
+                    bCount+=1
+        if aCount > bCount:
+            return True
+        return False
+
+    print(winnerOfGame(colors = "AAABABB"))
